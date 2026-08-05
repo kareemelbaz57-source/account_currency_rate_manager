adddfa3c80404342d41f1e04590379a110f5f572
kareemelbaz57-source <kareemelbaz57@gmail.com>
2026-08-05 08:56:35 +0300

    Update frankfurter_service.py

--- services/frankfurter_service.py
+++ services/frankfurter_service.py
@@ -7,12 +7,16 @@ class FrankfurterService:
 
     @classmethod
     def get_rates(cls, base_currency):
-        response = requests.get(
-            cls.BASE_URL,
-            params={"from": base_currency},
-            timeout=20,
-        )
+        try:
+            response = requests.get(
+                cls.BASE_URL,
+                params={"from": base_currency},
+                timeout=20,
+            )
 
-        response.raise_for_status()
+            response.raise_for_status()
 
-        return response.json().get("rates", {})
+            return response.json().get("rates", {})
+
+        except requests.RequestException:
+            return {}
