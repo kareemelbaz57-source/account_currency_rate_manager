e79a095a1007ab706ebb5bda319b72ff5b862256
kareemelbaz57-source <kareemelbaz57@gmail.com>
2026-08-05 08:26:22 +0300

    Update res_currency.py
    Refactor res_currency model

--- models/res_currency.py
+++ models/res_currency.py
@@ -1,5 +1,8 @@
 from odoo import api, fields, models
+
 from ..services.frankfurter_service import FrankfurterService
+from ..services.ecb_service import ECBService
+
 
 class ResCurrency(models.Model):
     _inherit = "res.currency"
@@ -37,77 +40,80 @@ class ResCurrency(models.Model):
             currency.current_rate = rate.rate if rate else 0.0
 
     def action_update_manual_rate(self):
-    self.ensure_one()
+        self.ensure_one()
 
-    return {
-        "type": "ir.actions.act_window",
-        "name": "Update Currency Rate",
-        "res_model": "currency.rate.update.wizard",
-        "view_mode": "form",
-        "target": "new",
-        "context": {
-            "default_currency_id": self.id,
-            "default_company_id": self.env.company.id,
-            "default_rate": self.current_rate or 1.0,
-        },
-    }
-        for currency in self:
-            if currency.manual_rate <= 0:
+        return {
+            "type": "ir.actions.act_window",
+            "name": "Update Currency Rate",
+            "res_model": "currency.rate.update.wizard",
+            "view_mode": "form",
+            "target": "new",
+            "context": {
+                "default_currency_id": self.id,
+                "default_company_id": self.env.company.id,
+                "default_rate": self.current_rate or 1.0,
+            },
+        }
+
+    def action_auto_update_rates(self):
+        provider = self.env["ir.config_parameter"].sudo().get_param(
+            "account_currency_rate_manager.provider",
+            default="manual",
+        )
+
+        if provider == "manual":
+            return True
+
+        company = self.env.company
+        base_currency = company.currency_id.name
+
+        if provider == "frankfurter":
+            rates = FrankfurterService.get_rates(base_currency)
+        elif provider == "ecb":
+            rates = ECBService.get_rates(base_currency)
+        else:
+            return True
+
+        for currency in self.search([]):
+            if currency.name not in rates:
                 continue
 
-            self.env["res.currency.rate"].create({
-                "currency_id": currency.id,
-                "company_id": self.env.company.id,
-                "name": fields.Date.today(),
-                "rate": currency.manual_rate,
-            })
+            old_rate = currency.current_rate
+
+            existing_rate = self.env["res.currency.rate"].search(
+                [
+                    ("currency_id", "=", currency.id),
+                    ("company_id", "=", company.id),
+                    ("name", "=", fields.Date.today()),
+                ],
+                limit=1,
+            )
+
+            if existing_rate:
+                existing_rate.write({
+                    "rate": rates[currency.name],
+                })
+            else:
+                self.env["res.currency.rate"].create({
+                    "currency_id": currency.id,
+                    "company_id": company.id,
+                    "name": fields.Date.today(),
+                    "rate": rates[currency.name],
+                })
 
             currency.write({
+                "manual_rate": rates[currency.name],
                 "last_update": fields.Datetime.now(),
                 "updated_by": self.env.user.id,
             })
 
+            self.env["currency.rate.history"].create({
+                "currency_id": currency.id,
+                "company_id": company.id,
+                "old_rate": old_rate,
+                "new_rate": rates[currency.name],
+                "update_type": "automatic",
+                "updated_by": self.env.user.id,
+            })
+
         return True
-
-   def action_auto_update_rates(self):
-       provider = self.env["ir.config_parameter"].sudo().get_param(
-    "account_currency_rate_manager.provider",
-    default="manual",
-)
-
-if provider != "frankfurter":
-    return True
-    company = self.env.company
-
-    base_currency = company.currency_id.name
-
-    rates = FrankfurterService.get_rates(base_currency)
-
-    for currency in self.search([]):
-
-        if currency.name not in rates:
-            continue
-
-        self.env["res.currency.rate"].create({
-            "currency_id": currency.id,
-            "company_id": company.id,
-            "name": fields.Date.today(),
-            "rate": rates[currency.name],
-        })
-
-        currency.write({
-            "manual_rate": rates[currency.name],
-            "last_update": fields.Datetime.now(),
-            "updated_by": self.env.user.id,
-        })
-
-        self.env["currency.rate.history"].create({
-            "currency_id": currency.id,
-            "company_id": company.id,
-            "old_rate": currency.current_rate,
-            "new_rate": rates[currency.name],
-            "update_type": "automatic",
-            "updated_by": self.env.user.id,
-        })
-
-    return True
