12f3661d4cbe1babd44192c9aad6bbdbf16e24b8
kareemelbaz57-source <kareemelbaz57@gmail.com>
2026-08-05 08:58:10 +0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -2,5 +2,6 @@ from . import res_currency
 from . import account_move
 from . import currency_rate_history
 from . import currency_rate_log
+from . import currency_dashboard
 from . import res_company
 from . import res_config_settings
