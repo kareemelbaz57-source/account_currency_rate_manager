7e36e984a737eb24138f904fb6b160950f353819
kareemelbaz57-source <kareemelbaz57@gmail.com>
2026-08-05 08:16:41 +0300

    Update res_currency.py
    Add Frankfurter API integration

--- models/res_currency.py
+++ models/res_currency.py
@@ -1,5 +1,5 @@
 from odoo import api, fields, models
-
+from ..services.frankfurter_service import FrankfurterService
 
 class ResCurrency(models.Model):
     _inherit = "res.currency"
@@ -69,9 +69,38 @@ class ResCurrency(models.Model):
 
         return True
 
-    def action_auto_update_rates(self):
-        """
-        Placeholder for automatic exchange rate update.
-        سيتم ربطها لاحقًا بمزود أسعار العملات (Frankfurter أو ECB أو أي API آخر).
-        """
-        return True
+   def action_auto_update_rates(self):
+    company = self.env.company
+
+    base_currency = company.currency_id.name
+
+    rates = FrankfurterService.get_rates(base_currency)
+
+    for currency in self.search([]):
+
+        if currency.name not in rates:
+            continue
+
+        self.env["res.currency.rate"].create({
+            "currency_id": currency.id,
+            "company_id": company.id,
+            "name": fields.Date.today(),
+            "rate": rates[currency.name],
+        })
+
+        currency.write({
+            "manual_rate": rates[currency.name],
+            "last_update": fields.Datetime.now(),
+            "updated_by": self.env.user.id,
+        })
+
+        self.env["currency.rate.history"].create({
+            "currency_id": currency.id,
+            "company_id": company.id,
+            "old_rate": currency.current_rate,
+            "new_rate": rates[currency.name],
+            "update_type": "automatic",
+            "updated_by": self.env.user.id,
+        })
+
+    return True
