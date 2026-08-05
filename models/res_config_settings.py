990dd3097ee1947dd4b25f0aeab43603fe92c362
kareemelbaz57-source <kareemelbaz57@gmail.com>
2026-08-05 08:21:17 +0300

    Update res_config_settings.py

--- models/res_config_settings.py
+++ models/res_config_settings.py
@@ -4,15 +4,16 @@ from odoo import fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
-    currency_rate_provider = fields.Selection(
-        [
-            ("manual", "Manual"),
-            ("frankfurter", "Frankfurter"),
-        ],
-        string="Currency Provider",
-        config_parameter="account_currency_rate_manager.provider",
-        default="manual",
-    )
+   currency_rate_provider = fields.Selection(
+    [
+        ("manual", "Manual"),
+        ("frankfurter", "Frankfurter"),
+        ("ecb", "European Central Bank"),
+    ],
+    string="Currency Provider",
+    config_parameter="account_currency_rate_manager.provider",
+    default="manual",
+)
 
     currency_auto_update = fields.Boolean(
         string="Automatic Currency Update",
