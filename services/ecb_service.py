da615ccdf8ba6dc018b7ae3570b98e5f3660e3a1
kareemelbaz57-source <kareemelbaz57@gmail.com>
2026-08-05 08:20:22 +0300

    Create ecb_service.py

---
+++ services/ecb_service.py
@@ -0,0 +1,18 @@
+import requests
+
+
+class ECBService:
+
+    BASE_URL = "https://api.frankfurter.app/latest"
+
+    @classmethod
+    def get_rates(cls, base_currency):
+        response = requests.get(
+            cls.BASE_URL,
+            params={"from": base_currency},
+            timeout=20,
+        )
+
+        response.raise_for_status()
+
+        return response.json().get("rates", {})
